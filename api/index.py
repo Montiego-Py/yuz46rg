0cfbcbc0119aca1b6380717115c7f5665faf16bf
Montiego-Py <suvermezaslan69@gmail.com>
2026-01-17 03:31:47 +0300

    Refactor HTTP handler to a Vercel function format

--- api/index.py
+++ api/index.py
@@ -1,14 +1,12 @@
-from http.server import BaseHTTPRequestHandler
 import json
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "application/json")
-        self.end_headers()
-
-        response = {
+def handler(request):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
             "message": "Python Vercel function çalışıyor 🚀"
-        }
-
-        self.wfile.write(json.dumps(response).encode())
+        })
+    }
